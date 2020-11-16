21edaf372b76f42f566f15d6c3b440c8f971e615
emilyrr-stanford <74217424+emilyrr-stanford@users.noreply.github.com>
2020-11-16 10:22:34 -0800

    Hide breadcrumbs and side menu on home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-    function ShowOrHideSideNav(){
-        if (window.location.pathname == "/ophthalmic-ao-handbook/"){
-            $('#colSidenav').addClass('d-none');
-            $('#colContent').removeClass('col-lg-9');
-            $('#breadcrumbHome').addClass('d-none');
-        }
-    };
-    ShowOrHideSideNav();
+
 
 });
